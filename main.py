6b3d799cce9b2948d3fa9477758a771ffeff8737
Chetan Ruparel <chetan@Chetans-iMac.local>
2019-04-03 00:23:46 +0530

    added functionality of listing all CSV in dropdown

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template
+import os
+import json
+import datetime
+import pymongo
+from flask import jsonify
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return("Hello World!")
+    return render_template('index.html')
+
+@app.route("/apiListCSV")
+def listing():
+    client = pymongo.MongoClient('127.0.0.1', 27017)
+    db = client.csvProject
+    collection = db.masterCSV
+    output = []
+    for cursor in collection.find():
+        output.append({'title':cursor['title'],'list':cursor['listOfCSV']})
+        return jsonify(output)
+
+    return("some json")
 
 if __name__ == "__main__":
-    app.run(host= '0.0.0.0',port=5000)
+    app.run(debug=True)
