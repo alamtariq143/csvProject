b6edb0bfb62091d64c3682ca557186a06c653f79
Chetan Ruparel <chetan@Chetans-iMac.local>
2019-04-04 00:08:16 +0530

    Code before pushing to production

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import os
 import json
 import datetime
 import pymongo
 from flask import jsonify
+from bson import json_util
 
 app = Flask(__name__)
 
@@ -22,6 +23,28 @@ def listing():
         return jsonify(output)
 
     return("some json")
+
+@app.route("/csv")
+def csvDisplay():
+    client = pymongo.MongoClient('127.0.0.1', 27017)
+    db = client.csvProject
+    csvFile = request.args.get('csvName')
+    print(csvFile)
+    csvName = csvFile.split('.')
+    csvCollectionName = csvName[0]
+    print(csvCollectionName)
+    collectionList = db.list_collection_names()
+    output = []
+
+    if csvCollectionName in collectionList:
+        print("The collection exists.")
+        collection = db[csvCollectionName]
+        objects = collection.find({}, {'_id': False})
+        return str(json.dumps({'results': list(objects)}, default = json_util.default, indent = 4))
+        for x in collection.find():
+            print(x)
+            output.append(x)
+    return("output")
 
 if __name__ == "__main__":
     app.run(debug=True)
